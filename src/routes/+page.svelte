c7f256de45b0da80a3f85dfc7d1d815ea663791c
mordocconicole <nicole.mordocco@mail.polimi.it>
2026-04-08 17:20:28 +0200

    bind controls to props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <script>
+    import CanvasSquare from "$lib/components/CanvasSquare.svelte";
     import ClicksCounter from "$lib/components/ClicksCounter.svelte";
-    console.log("hello world");
+
+
+    let count=$state(5)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter initialCount={0}/>
+<ClicksCounter bind:initialCount={count}/>
+
+<p>count from parent is {count}</p>
+
+<CanvasSquare/>
+
 <style>
     h1 {
         color: deepskyblue;
